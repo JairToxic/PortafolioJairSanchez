787fdd740b682c70cbb7ae3b47c71b4946f5711e
JairToxic <alexis151270sanchez@gmail.com>
2024-08-24 21:28:57 -0500

    feat:bug

--- components/contactame.tsx
+++ components/contactame.tsx
@@ -80,7 +80,7 @@ const ContactMe: React.FC = () => {
                             value={formData.message}
                             onChange={handleChange}
                             className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            rows="4"
+                            rows={4}
                             required
                         ></textarea>
                     </div>
