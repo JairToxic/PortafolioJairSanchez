84f4abb75570bb9e04c1fd3b1b9f482842ddc062
JairToxic <alexis151270sanchez@gmail.com>
2024-08-24 22:46:08 -0500

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ import Header from "@/components/header";
 const urbanist = Urbanist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "TarreDev Landing Page",
-  description: "Landing page made by TarreDev",
+  title: "Jair Sanchez Portafolio",
+  description: "Landing page made by Jair Sanchez",
 };
 
 export default function RootLayout({
