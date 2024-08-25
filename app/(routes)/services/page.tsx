ea55f49a2a45a218157dfef4983d5991c19b3e8f
JairToxic <alexis151270sanchez@gmail.com>
2024-08-24 22:43:05 -0500

    feat:bug servicios

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -6,23 +6,33 @@ import TransitionPage from "@/components/transition-page";
 const ServicesPage = () => {
     return (
         <>
-
             <TransitionPage />
             <CircleImage />
             <AvatarServices />
             <div className="grid items-center justify-center h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2">
-                <div className="max-w-[450px]">
-
-                    <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">Mis <span className="font-bold text-secondary"> servicios.</span></h1>
-                    <p className="mb-3 text-xl text-gray-300">Ofrezco servicios de desarrollo web frontend especializados en la creación de sitios web y aplicaciones atractivas y funcionales. Utilizando las últimas tecnologías, como HTML, CSS y JavaScript,React, NextJs, Boostrap, Tailwind, Python  diseño interfaces de usuario intuitivas y responsivas que reflejan la identidad de marca de mis clientes y mejoran su presencia en línea. Abordando siempre la seguridad y dandoles toques extras con Inteligencia Artificial.</p>
+                <div className="max-w-[450px] mt-40 md:mt-0 space-y-6 mb-16 md:mb-0"> {/* Añade margen inferior en pantallas pequeñas */}
+                    <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">
+                        Mis <span className="font-bold text-secondary"> servicios.</span>
+                    </h1>
+                    <p className="text-xl text-gray-300">
+                        Ofrezco servicios de desarrollo web frontend especializados en la creación de sitios web y aplicaciones atractivas y funcionales. Utilizando las últimas tecnologías, como HTML, CSS y JavaScript, React, NextJs, Bootstrap, Tailwind, Python, diseño interfaces de usuario intuitivas y responsivas que reflejan la identidad de marca de mis clientes y mejoran su presencia en línea. Abordando siempre la seguridad y dándoles toques extras con Inteligencia Artificial.
+                    </p>
                     <button className="px-3 py-2 rounded-lg bg-secondary hover:bg-secondary/65">Contacta conmigo</button>
                 </div>
 
                 {/* SLIDER */}
                 <div>
                     <SliderServices />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
                 </div>
+                
             </div>
+            
         </>
     );
 }
